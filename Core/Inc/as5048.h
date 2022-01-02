33183179c3142d7e9940082ff23acabd2b31199b
Bauman Robotics | BMSTU <baumanrobotics@gmail.com>
2022-01-03 00:05:56 +0300

    create

---
+++ Core/Inc/as5048.h
@@ -0,0 +1,9 @@
+#ifndef __AS5048_H__
+#define __AS5048_H__
+
+#include "stm32f1xx.h"
+#include "main.h"
+
+float get_angle(void);
+
+#endif /* __AS5048_H__ */
---
+++ keil_as5048a_f103/MDK-ARM/as5048a_f103/cobs.d
@@ -0,0 +1,4 @@
+as5048a_f103\cobs.o: ..\Core\Src\cobs.c
+as5048a_f103\cobs.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+as5048a_f103\cobs.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+as5048a_f103\cobs.o: ../Core/Inc/cobs.h
